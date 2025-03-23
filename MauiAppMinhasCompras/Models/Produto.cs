1979f8992f11102d05d2fdf75128bc5f0802de3e
sargentini <guilhermesargentini@gmail.com>
2025-03-23 17:04:03 -0300

    atualização ag06

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,51 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Descrição não pode ser vazia");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("A quantidade não pode ser 0");
+                }
+
+                _quantidade = value;
+            }
+        }
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("O preço não pode ser 0");
+                }
+
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
 
     }
